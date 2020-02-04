273f41a22948060a7adc8326f74370495f93902d
Misha Kushka <kushka.misha@gmail.com>
2020-02-04 21:27:14 +0200

    Finish Login screen

---
+++ crypto-chat/Extensions/MyButton.swift
@@ -0,0 +1,27 @@
+//
+//  MyButton.swift
+//  crypto-chat
+//
+//  Created by Kushka Misha on 2/4/20.
+//  Copyright © 2020 Misha Kushka. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class MyButton: UIButton {
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setupView()
+    }
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setupView()
+    }
+    
+    private func setupView() {
+        backgroundColor = UIColor.white
+        cornerRadius = 10
+    }
+}
