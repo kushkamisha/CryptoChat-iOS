843cd08f620b67c5b516972c8f5a1a805f595b2e
Misha Kushka <kushka.misha@gmail.com>
2020-02-04 21:03:37 +0200

    Add pass field

--- LoginViewController.swift
+++ LoginViewController.swift
@@ -11,12 +11,14 @@ import UIKit
 class LoginViewController: UIViewController {
 
     @IBOutlet weak var emailInputLabel: UIView!
+    @IBOutlet weak var passInputLabel: UIView!
     @IBOutlet weak var signInBigLabel: UILabel!
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
         emailInputLabel.roundCorners(corners: [.topLeft, .bottomLeft], radius: 10)
+        passInputLabel.roundCorners(corners: [.topLeft, .bottomLeft], radius: 10)
         signInBigLabel.transform = CGAffineTransform(rotationAngle: -CGFloat.pi / 2)
     }
 
