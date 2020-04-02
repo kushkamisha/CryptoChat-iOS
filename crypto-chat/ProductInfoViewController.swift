a7088f9633b37c32a32ce36c2597d1cba3a81003
Misha Kushka <kushka.misha@gmail.com>
2020-04-02 20:47:03 +0300

    Add developer info screen

---
+++ crypto-chat/ProductInfoViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ProductInfoViewController.swift
+//  crypto-chat
+//
+//  Created by Kushka Misha on 4/2/20.
+//  Copyright © 2020 Misha Kushka. All rights reserved.
+//
+
+import UIKit
+
+class ProductInfoViewController: UIViewController {
+
+    override var preferredStatusBarStyle : UIStatusBarStyle {
+        return UIStatusBarStyle.lightContent
+    }
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    }
+
+}
